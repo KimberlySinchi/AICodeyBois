cb46fee3f360ca7b64d422a174813982ec819d6b
Elizabeth-12324 <41752522+Elizabeth-12324@users.noreply.github.com>
2019-01-12 04:42:56 -0500

    updates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/DriverModeAuto.java
@@ -0,0 +1,276 @@
+/*
+Copyright 2018 FIRST Tech Challenge Team 12178
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR O`THER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode.Code;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Helpers.Slave;
+import org.firstinspires.ftc.teamcode.Helpers.SlaveAuto;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
+@TeleOp(name = "DriveData", group = "TeleOp")
+
+public class DriverModeAuto extends OpMode {
+
+    /* Declare OpMode members. */
+    private SlaveAuto slave = new SlaveAuto();
+
+    @Override
+    public void init()
+    {
+        slave.init(hardwareMap);
+        telemetry.addLine(slave.getStatus());
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop()
+    {
+        telemetry.addData("Status ", "WORKING");
+        telemetry.update();
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start()
+    {
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop()
+    {
+        double bufferA = 5;
+        double rightPress = gamepad1.right_trigger;
+        double leftPress = gamepad1.left_trigger * -1;
+        double x2 = gamepad2.left_stick_x;
+        double y2 = gamepad2.left_stick_y;
+        boolean dPadUp = gamepad1.dpad_up;
+        boolean dPadDown = gamepad1.dpad_down;
+
+        boolean resetEncod = gamepad1.back;
+
+        double y = gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double yRight = gamepad1.right_stick_y;
+        double xLeft = gamepad1.right_stick_x;
+        telemetry.addData("Status:","x = " + x + " ,y =  " +y  );
+        telemetry.update();
+        double theta = Math.atan(y/x);
+        String compare = "-0.0";
+        String sTheta = "" + theta;
+        boolean isnegZ = true;
+        for(int i = 0; i<4; i++)
+        {
+            if((compare.charAt(i) + "").equals(sTheta.charAt(i) + "") == false)
+            {
+                isnegZ = false;
+                i = 10;
+            }
+        }
+        if(isnegZ == true && x<0)
+        {
+            theta = Math.PI;
+        }
+        //Q2
+        else if(y>0 && x<0)
+        {
+            theta = Math.PI + theta;
+            telemetry.addData("Q2", "");
+
+        }
+        //Q3
+        else if((x>=-1 && x<0) && (y<0 && y>=-1))
+        {
+            theta += Math.PI;
+            telemetry.addData("Q3","");
+        }
+        //Q4
+        else if((x>0 && x<=1) && (y<0 && y>=-1))
+        {
+            theta = 2*Math.PI + theta;
+            telemetry.addData("Q4", "");
+        }
+        //Q1
+        else
+        {
+            theta = theta;
+            telemetry.addData("Q1", "");
+        }
+        double degree = 0;
+        degree = (theta/Math.PI) * 180;
+        telemetry.addData("Status:","degree = " + degree);
+
+        double rad = Math.sqrt(x*x + y*y);
+        if(rad > 1)
+        {
+            rad = 1;
+        }
+        double v1 = rad*(-1*Math.sin(theta + Math.PI/4));
+        double v2 = rad*Math.cos(theta + Math.PI/4);
+        telemetry.addData("v1 = " + v1, " v2 = " + v2);
+
+        //Rotate right
+        if(rightPress != 0)
+        {
+            /*
+            frontL.setPower(-1*rightPress);
+            frontR.setPower(-1*rightPress);
+            backR.setPower(-1*rightPress);
+            backL.setPower(-1*rightPress);
+            */
+            slave.frontL.setPower(-1*rightPress);
+            slave.frontR.setPower(-1*rightPress);
+            slave.backR.setPower(-1*rightPress);
+            slave.backL.setPower(-1*rightPress);
+        }
+        //Rotate left
+        else if(leftPress != 0)
+        {
+            /*
+            frontL.setPower(-1*leftPress);
+            frontR.setPower(-1*leftPress);
+            backR.setPower(-1*leftPress);
+            backL.setPower(-1*leftPress);
+            */
+            slave.frontL.setPower(-1*leftPress);
+            slave.frontR.setPower(-1*leftPress);
+            slave.backR.setPower(-1*leftPress);
+            slave.backL.setPower(-1*leftPress);
+        }
+        else if(x==0 && y==0)
+        {
+            /*
+            frontL.setPower(0);
+            frontR.setPower(0);
+            backR.setPower(0);
+            backL.setPower(0);
+            */
+            slave.frontL.setPower(0);
+            slave.frontR.setPower(0);
+            slave.backR.setPower(0);
+            slave.backL.setPower(0);
+        }
+        else
+        {
+            /*
+            frontL.setPower(-v2);
+            frontR.setPower(v1);
+            backR.setPower(v2);
+            backL.setPower(-v1);
+            */
+            slave.frontL.setPower(-v2);
+            slave.frontR.setPower(v1);
+            slave.backR.setPower(v2);
+            slave.backL.setPower(-v1);
+        }
+        //Moving the arm up
+        /**
+                if(yRight < 0)
+        {
+            armMotor1.setPower(y);
+            armMotor2.setPower(-y);
+            telemetry.addLine("Arm Up");
+        }
+        //Moving the arm down
+        else if(yRight > 0)
+        {
+            armMotor1.setPower(-y);
+            armMotor2.setPower(y);
+            telemetry.addLine("Arm Down");
+        }
+        //No vertical arm movement
+        else
+        {
+            armMotor1.setPower(0);
+            armMotor2.setPower(0);
+        }
+        telemetry.update();
+**/
+        //SERVOS
+        if(dPadUp)
+            slave.armIntake.setPosition(1);
+        else if(dPadDown)
+            slave.armIntake.setPosition(0);
+        else
+            slave.armIntake.setPosition(0.5);
+
+        //upDownMotor = name of the motor that controls vertical arm movement
+        //up = the y2 might have to be changed
+        /**
+        if(y2<0)
+        {
+            upDownMotor.setPower(y2);
+        }
+        else
+        {
+            upDownMotor.setPower(0);
+        }
+        if(y2>0)
+        {
+            upDownMotor.setPower(-y2);
+        }
+        else
+        {
+            upDownMotor.setPower(0);
+        }
+         **/
+        if(yRight>0)
+            slave.latch.setPower(-.9);
+        else if(yRight<0)
+            slave.latch.setPower(.9);
+        else
+            slave.latch.setPower(0);
+        slave.latch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slave.latch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("Latch Pos: ",slave.latch.getCurrentPosition());
+        telemetry.update();
+
+        if(resetEncod)
+            slave.latch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        else
+            slave.latch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/dogeAuto6.java
@@ -0,0 +1,512 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Code;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Helpers.SlaveAuto;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ * <p>
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ * <p>
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name = "AWESOMEAUTOREVERSE(plsWork)", group = "Linear Opmode")
+public class dogeAuto6 extends LinearOpMode {
+
+    // Declare OpMode members.
+    // Detector object
+    public SlaveAuto slave = new SlaveAuto();
+    private boolean bool1 = true;
+    private boolean bool2 = true;
+    private boolean bool3 = true;
+    private GoldAlignDetector detector;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontL;
+    private DcMotor frontR;
+    private DcMotor backL;
+    private DcMotor backR;
+    private DcMotor latch;
+    private Servo armSpin;
+    static final double SPEED = 1;
+
+    public boolean isBlock() {
+        return detector.getAligned();
+    }
+
+    @Override
+    public void runOpMode() {
+        // Set up detector
+        detector = new GoldAlignDetector(); // Create detector
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+        detector.useDefaults(); // Set detector to use default settings
+
+        // Optional tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.8; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005; //
+
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+        detector.enable(); // Start the detector!
+        /**
+         try {
+         frontL = hardwareMap.get(DcMotor.class, "DC1");
+         frontL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+         } catch (Exception e) {
+         }
+         try {
+         frontR = hardwareMap.get(DcMotor.class, "DC2");
+         frontR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         } catch (Exception e) {
+         }
+         try {
+         backR = hardwareMap.get(DcMotor.class, "DC3");
+         backR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         } catch (Exception e) {
+         }
+         try {
+         backL = hardwareMap.get(DcMotor.class, "DC4");
+         backL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         } catch (Exception e) {
+         }
+         **/
+        slave.init(hardwareMap);
+        telemetry.addLine(slave.getStatus());
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            if (bool1) {
+                /**
+                 int timeUntilFirstBlock = 3;
+                 double timeLeft = rotateLeft(7, .5);
+                 //goUp(1.3); //in the future go back: goBack(1.5); //1.3 seconds with full power
+                 //goBack(1.3);//1.3 seconds with full power
+                 goUp(2.6,.5);//half power, so double time for equal distance (test)
+                 motorsOff(.6);
+                 goBack(2.6,.5);//half power, so double time for equal distance(test)
+                 motorsOff(.3);
+                 rotateRightB(.5,timeLeft - timeUntilFirstBlock);
+                 //make sure this works, then continue.
+                 **/
+                //NEW CODE USING MEASUREMENTS AND VELOCITIES
+                latchDown(3.5, .9);
+                double timeUntilFirstBlock = angleConv(83); //angles only at .3
+                goUp(forwardConv(10), .6);
+                double timeLeft = rotateLeft(7, .15);
+                /**
+                 goUp(forwardConv(70), .6);
+                 motorsOff(.6);
+                 goDown(forwardConv(42), .6);
+                 motorsOff(.6);
+                 goUp(forwardConv(26.29),.6);
+                 motorsOff(.6);
+                 rotateRightB(.3,angleConv(88.99));
+                 motorsOff(.6);
+                 goUp(forwardConv(31.36),.6);
+                 motorsOff(.6);
+                 rotateRightB(.3,angleConv(127.69));
+                 motorsOff(.6);
+                 goUp(forwardConv(39.98),.6);
+                 motorsOff(.6);
+                 rotateLeftB(.3,angleConv(171.44));
+                 motorsOff(.6);
+                 goUp(forwardConv(71.89),.6);
+                 /* *
+                 *
+                 */
+                timeUntilFirstBlock = angleConv(83); //angles only at .3
+                goUp(forwardConv(10), .6);
+                timeLeft = rotateLeft(7, .15);
+                goUp(forwardConv(70), .6);
+                motorsOff(.6);
+                goDown(forwardConv(42), .6);
+                motorsOff(.6);
+                rotateRightB(.11, timeLeft - 2.61);
+                motorsOff(.4);
+                rotateLeftB(.15, 2.31);
+                goUp(forwardConv(80), .6);
+                rotateLeftB(.15, 2.6);
+                goDown(forwardConv(65.5), .6);
+                motorsOff(.6);
+                goUp(forwardConv(63.6), .6);
+                rotateLeftB(angleConv(98), .15);//this is in degrees ==> it will rotate this many degrees, given that the power is .3
+                //Stuff to test for velocity:
+
+                //rotateRightB(.15,10); //at .15 power the robot needs 9 seconds to go one revolution
+                //motorsOff(10);
+                //goUp(10,.6);
+
+
+                bool1 = false;
+            }
+            detector.disable();
+        }
+    }
+
+    public void rotateRightB(double power, double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        boolean totalTime = false;
+        double newTime = time * -1;
+        if (time > 0) {
+            while (opModeIsActive() && ms.time() <= time) {
+                frontL.setPower(-power);
+                frontR.setPower(-power);
+                backR.setPower(-power);
+                backL.setPower(-power);
+
+            }
+        } else {
+            while (opModeIsActive() && ms.time() <= time) {
+                frontL.setPower(power);
+                frontR.setPower(power);
+                backR.setPower(power);
+                backL.setPower(power);
+
+            }
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void rotateLeftB(double power, double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        double newTime = time * -1;
+        boolean totalTime = false;
+        if (time > 0) {
+            while (ms.time() <= time) {
+                frontL.setPower(power);
+                frontR.setPower(power);
+                backR.setPower(power);
+                backL.setPower(power);
+
+            }
+        } else {
+            while (ms.time() <= time) {
+                frontL.setPower(-power);
+                frontR.setPower(-power);
+                backR.setPower(-power);
+                backL.setPower(-power);
+            }
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void motorsOff(double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(0.0);
+            frontR.setPower(0.0);
+            backR.setPower(0.0);
+            backL.setPower(0.0);
+        }
+
+    }
+
+    public void goRightB() {
+    }
+
+    public void goLeftB() {
+    }
+
+    public void goUp(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(-power);
+            frontR.setPower(power);
+            backR.setPower(power);
+            backL.setPower(-power);
+        }
+        frontL.setPower(0.0);
+        frontR.setPower(0.0);
+        backR.setPower(0.0);
+        backL.setPower(0.0);
+    }
+
+    public void goDown(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(power);
+            frontR.setPower(-power);
+            backR.setPower(-power);
+            backL.setPower(power);
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void goLeft(double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(-1);
+            frontR.setPower(1);
+            backR.setPower(-1);
+            backL.setPower(1);
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void goRight(double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(1);
+            frontR.setPower(-1);
+            backR.setPower(1);
+            backL.setPower(-1);
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void rotateCWise(double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(1);
+            frontR.setPower(1);
+            backR.setPower(1);
+            backL.setPower(1);
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void rotateCCWise(double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(-1);
+            frontR.setPower(-1);
+            backR.setPower(-1);
+            backL.setPower(-1);
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+    }
+
+    public void rotateCWiseNoNum(double power) {
+        frontL.setPower(power);
+        frontR.setPower(power);
+        backR.setPower(power);
+        backL.setPower(power);
+    }
+
+    public void rotateCCWiseNoNum(double power) {
+        frontL.setPower(-power);
+        frontR.setPower(-power);
+        backR.setPower(-power);
+        backL.setPower(-1 * power);
+    }
+
+    public void rotateSTOP(double time) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(0);
+            frontR.setPower(0);
+            backR.setPower(0);
+            backL.setPower(0);
+        }
+    }
+
+    public boolean isAligned() {
+        return detector.getAligned();
+    }
+
+    public double rotateLeft(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time && isAligned() == false) {
+            frontL.setPower(power);
+            frontR.setPower(power);
+            backR.setPower(power);
+            backL.setPower(power);
+
+        }
+
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+        if (ms.time() <= time) {
+            return ms.time();
+        }
+        return time;//LEFT IS POSITIVE TIME, RIGHT IS NEGATIVE TIME
+    }
+
+
+    public double rotateRight(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        boolean totalTime = false;
+        while (ms.time() <= time && isAligned() == false) {
+            frontL.setPower(-power);
+            frontR.setPower(-power);
+            backR.setPower(-power);
+            backL.setPower(-power);
+            if (time == ms.time()) {
+                totalTime = true;
+            }
+        }
+        frontL.setPower(0);
+        frontR.setPower(0);
+        backR.setPower(0);
+        backL.setPower(0);
+        if (ms.time() <= time) {
+            return -1 * ms.time();
+        }
+        if (totalTime) {
+            return -1 * time;
+        }
+        return 0;
+    }
+
+    public void goBack(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            frontL.setPower(power);
+            frontR.setPower(-power);
+            backR.setPower(-power);
+            backL.setPower(power);
+        }
+        frontL.setPower(0.0);
+        frontR.setPower(0.0);
+        backR.setPower(0.0);
+        backL.setPower(0.0);
+    }
+
+    public void Stop() {
+        detector.disable();
+    }
+
+    public double forwardConv(double length) //length is in cm
+    {
+        //forward by a power of: .6
+        //velocity is 39.37cm/seconds
+        return length / 39.37;
+
+    }
+
+    public double rotateConv(double length) {
+        //rotational power: .6
+        //velocity: 5.48 cm/sec
+        return length / 5.48;
+    }
+
+    public double angleConv(double angle) //at .35 power : 6 sec = 1 rev, 6sec = 360 degree
+    {
+        return (angle / 360.0) * 9.5;
+    }
+
+    public double tileConv(double tiles)//1 tile is 62 cm this is at .6
+    {
+        return ((tiles) / 62) * 1.125;
+    }
+
+    public void off() {
+        frontL.setPower(0.0);
+        frontR.setPower(0.0);
+        backR.setPower(0.0);
+        backL.setPower(0.0);
+    }
+
+    public void latchUp(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            slave.latch.setPower(-1 * power);
+        }
+        frontL.setPower(0.0);
+        frontR.setPower(0.0);
+        backR.setPower(0.0);
+        backL.setPower(0.0);
+    }
+
+    public void latchDown(double time, double power) {
+        ElapsedTime ms = new ElapsedTime();
+        ms.reset();
+        while (ms.time() <= time) {
+            slave.latch.setPower(power);
+        }
+        frontL.setPower(0.0);
+        frontR.setPower(0.0);
+        backR.setPower(0.0);
+        backL.setPower(0.0);
+    }
+}
